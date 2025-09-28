416e8e1c60e02f36a158eab6b6aa0e7376f323d8
VegdaLeena456 <leenavegda49@gmail.com>
2025-09-28 15:26:59 +0530

    Add educator page

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <h1>Edu Footer</h1>
-    </div>
+    <Footer className="flex md:flow-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t" >
+   <div>
+    <img src={assets.logo} alt="Logo" className='hidden md:block' />
+    <div className='hidden md:block h-7 bg-gray-500/60'></div>
+    <p className='py-4 text-center text-xs '></p>
+    </div>   
+    </Footer>
   )
 }
 
--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRating, onRate}) => {
 
   const [rating, setRating] = useState(initialRating || 0)
+
+  const handleRating = (value) => {
+    setRating(value);
+    if(onRate) onRate(value)
+  } 
+
+  useEffect(()=>{
+    if(initialRating){
+      setRating(initialRating)
+    }
+  }, [initialRating]);
+
    return (
     <div>
         {Array.from({length: 5}, (_, index)=>{
-          const startValue = index + 1;
+          const starValue = index + 1;
           return (
-            <span key={index} className={`text-xl sm:text:2xl cursor-pointer transition-colors ${startValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`}>
+            <span key={index} className={`text-xl sm:text:2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`} onClick={()=> handleRating(starValue)}>
               &#9733;
             </span>
           )
