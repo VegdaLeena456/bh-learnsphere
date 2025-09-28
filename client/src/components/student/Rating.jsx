2d7bcb56eb9e240b6c863862f3c150edfc0eab03
VegdaLeena456 <leenavegda49@gmail.com>
2025-09-28 13:56:11 +0530

    Add Player and Rating

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Rating = () => {
-  return (
+
+  const [rating, setRating] = useState(initialRating || 0)
+   return (
     <div>
-        <h1>Rating</h1>
+        {Array.from({length: 5}, (_, index)=>{
+          const startValue = index + 1;
+          return (
+            <span key={index} className={`text-xl sm:text:2xl cursor-pointer transition-colors ${startValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`}>
+              &#9733;
+            </span>
+          )
+        })}
     </div>
   )
 }
